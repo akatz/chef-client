a7a3799f719f47f9f54b0333989b590b9ec46000
Avrohom Katz <iambpentameter@gmail.com>
2012-11-01 11:59:14 -0400

    remove extra comma typo

--- recipes/service.rb
+++ recipes/service.rb
@@ -89,7 +89,7 @@ when "init"
   dist_dir, conf_dir = value_for_platform_family(
     ["debian"] => ["debian", "default"],
     ["rhel"] => ["redhat", "sysconfig"],
-    ["suse"] => ["suse", "sysconfig"],
+    ["suse"] => ["suse", "sysconfig"]
   )
 
   template "/etc/init.d/chef-client" do
